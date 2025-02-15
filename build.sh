25f59640008d66e9074a09221811b0ba39ad632a
Burda Boti <burdabotond@gmail.com>
2025-02-15 19:49:54 +0100

    build.sh modositas

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Move requirements.txt to the correct location if it's not there
+if [ ! -f "portofino/requirements.txt" ]; then
+    mv requirements.txt portofino/ 2>/dev/null || true
+fi
+
 cd portofino
 pip install -r requirements.txt
 
