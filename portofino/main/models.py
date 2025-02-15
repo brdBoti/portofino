97f7c33dcf202479bf595976ad1c42e502bf597c
Burda Boti <burdabotond@gmail.com>
2025-02-15 19:32:16 +0100

    pillow geci fos error javitva

--- portofino/main/models.py
+++ portofino/main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='employees/', null=True, blank=True)
+    image = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class Employee(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    cover_image = models.ImageField(upload_to='books/', null=True, blank=True)
+    cover_image = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -30,7 +30,7 @@ class Publication(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    cover_image = models.ImageField(upload_to='publications/', null=True, blank=True)
+    cover_image = models.URLField(null=True, blank=True)
     stock = models.IntegerField(default=0)
     
     def __str__(self):
